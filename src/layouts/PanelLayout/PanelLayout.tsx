cb0de98108d8ea028a54b6ea6bad217050c4069c
siavash <siavash.khani200@gmail.com>
2023-12-08 14:07:05 +0330

    feat(panelLayout): fixed dark theme

--- src/layouts/PanelLayout/PanelLayout.tsx
+++ src/layouts/PanelLayout/PanelLayout.tsx
@@ -2,10 +2,14 @@ import { Outlet } from "react-router-dom";
 import { Configurator } from "../../components";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import { useMaterialTailwindController } from "context";
 
 const PanelLayout = () => {
+  const [controller]: any = useMaterialTailwindController();
+  const { sidenavType } = controller;
+
   return (
-    <div>
+    <div className={`h-[100vh] ${sidenavType === "dark" ? "bg-[#111]" : "bg-[#e9e9e9]"}`}>
       <Sidebar />
       <div className="p-4 xl:ml-80">
         <Configurator />
